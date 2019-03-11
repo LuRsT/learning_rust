1b59b61f71316e520ad6ca79a9acb1abba8b4556
Gil Gonçalves <lursty@gmail.com>
2019-03-11 20:16:51 +0000

    Add fizzbuzz

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# learning_rust
+# Learning Rust
+
+This is a repo with some simple exercises I'm doing to learn Rust.
+
---
+++ fizzbuzz/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Fizz Buzz");
+    let mut i = 1;
+
+    while i < 101 {
+        if i % 15  == 0 {
+            println!("FizzBuzz");
+        }
+        else if i % 3  == 0 {
+            println!("Fizz");
+        }
+        else if i % 5 == 0 {
+            println!("Buzz");
+        }
+        else {
+            println!("{}", i);
+        }
+        i+=1;
+    }
+}
